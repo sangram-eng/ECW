e3d4529e472c8ae520c386ec3c6ea185f9244c62
Sangram Nayak <sangramnay421@gmail.com>
2024-01-29 23:06:31 +0530

    All project

---
+++ src/main/java/com/ecw/EcwApplication.java
@@ -0,0 +1,13 @@
+package com.ecw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecw/EcwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
